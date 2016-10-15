84530cdfee246da1287105c884cf28752cf513ca
NotDavvid <notdavvid@gmail.com>
2016-10-15 04:11:28 -0400

    home done

--- home/index.php
+++ home/index.php
@@ -14,10 +14,10 @@
     </div>
     <div id="profile">Alex McCormick<img src="../resources/icon/editicon.png" id="icon"><br><hr> Inflammatory Breast Cancer<img src="../resources/icon/editicon.png" id="icon"><br><hr>From: 03/2013<img src="../resources/icon/editicon.png" id="icon"><br><hr>Female<img src="../resources/icon/editicon.png" id="icon"><br><hr>26-45<img src="../resources/icon/editicon.png" id="icon"><br><hr>Bio:<img src="../resources/icon/editicon.png" id="icon"></div>
     <div id="bot">
-    <div class="buttons" id="left">
+    <div class="buttons" id="left" onclick="location.href='../conversations/index.php';" style="cursor:pointer;">
       Conversation
     </div>
-      <div class="buttons" id="right">
+      <div class="buttons" id="right" onclick="location.href='http://ww5.komen.org/BreastCancer/FactsForLife.html';" style="cursor:pointer;">
       Information
     </div>
     </div>
