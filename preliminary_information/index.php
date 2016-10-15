146924986d7334b5274a4e0024ee89aa508c510a
NotDavvid <notdavvid@gmail.com>
2016-10-14 20:56:21 -0400

    finished the prelim version of the prelim_info page

--- preliminary_information/index.php
+++ preliminary_information/index.php
@@ -7,7 +7,37 @@
 </head>
 
   <body>
-
+    <form action="action_page.php">
+      Diagnosis Date:<br>
+      <input type="text" name="ddate" value="MM/YYYY">
+      <br>
+      Cancer Type
+      <br>
+      
+      
+      <input list="types" name="types">
+      <datalist id="types">
+        <option value="Noninvasive (in situ) breast cancer">
+        <option value="Invasive (infiltrating) breast cancer">
+        <option value="Invasive ductal carcinoma (IDC)">
+        <option value="Invasive lobular carcinoma (ILC)">
+        <option value="Ductal carcinoma in situ (DCIS)">
+        <option value="Invasive ductal carcinoma (IDC)">
+        <option value="Lobular carcinoma in situ (LCIS)">
+        <option value="Invasive lobular carcinoma (ILC)">
+        <option value="Inflammatory breast cancer">
+        <option value="Metastatic breast cancer">
+      </datalist><br>
+      
+      <input type="submit" value="Submit">
+      
+      
+      
+    </form>
+    
+    
+    
+  
 
   </body>
 
