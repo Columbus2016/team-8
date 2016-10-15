ef2628898de1b426974c01ededf46f731edd4121
NotDavvid <notdavvid@gmail.com>
2016-10-14 22:14:47 -0400

    changes

--- index.php
+++ index.php
@@ -7,10 +7,17 @@
 </head>
 
   <body>
-    <div class="show">
+    <div class="fullscreen-bg">
+    <video loop muted autoplay poster="resources/video/" class="fullscreen-bg__video">
+        <source src="video/big_buck_bunny.webm" type="video/webm">
+        <source src="video/big_buck_bunny.mp4" type="video/mp4">
+        <source src="video/big_buck_bunny.ogv" type="video/ogg">
+    </video>
+</div>
+    <div class="show" id="left">
       sign-up
     </div>
-    <div class="show">
+    <div class="show" id="right">
       login
     </div>
     <div id="hide">
