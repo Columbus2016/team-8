4b387243a6ce10df5f4049ee0118f8e88d6c917e
NotDavvid <notdavvid@gmail.com>
2016-10-15 08:22:50 -0400

    more

--- messager/index.php
+++ messager/index.php
@@ -9,6 +9,30 @@
   <body>
     <div id="header">Megan</div>
     <div id="chatbox"></div>
-    <div id="textfield"><input type="text" placeholder="..."><img src="../resources/icon/"</div>
+    <div id="textfield"><div onclick="create()"><input type="text" placeholder="..." id="inp" ><img src="../resources/icon/sendicon.png" width="90px"></div></div>
   </body>
+  <script>
+  function create() {
+    var msg = document.getElementById("inp").value;
+    document.getElementById("chatbox").innerHTML += "<div class=\"outer\"><div class=\"test\">"+ msg+"</div></div><br>";
+    document.getElementById("inp").value = "";
+  }
+    function create1() {
+      var msg = document.getElementById("inp").value;
+      var elm = document.getElementById("chatbox");
+      var newElement = document.createElement('div');
+      newElement.setAttribute("class", "test");
+      newElement.textContent= msg;
+      elm.appendChild(newElement);
+      document.getElementById("inp").value = "";
+
+    }
+    document.getElementById('inp').onkeypress = function(e) {
+    if(e.keyCode == 13) {
+      var msg = document.getElementById("inp").value;
+    document.getElementById("chatbox").innerHTML += "<div class=\"outer\"><div class=\"test\">"+ msg+"</div></div><br>";
+    document.getElementById("inp").value = "";
+    }
+}
+  </script>
 </html>
