fd5d75db95ad1e8992a015082f21e4803aa1adce
NotDavvid <notdavvid@gmail.com>
2016-10-15 03:23:28 -0400

    finished home page

--- home/index.php
+++ home/index.php
@@ -2,16 +2,19 @@
 <html>
 <head>
   <title>Home</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="home/style.css">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
 </head>
 
   <body>
-  
-    <div>
+    <div id="top">
+      Profile
+    </div>
+    <div class="buttons" id="left">
       Conversation
     </div>
-    <div>
+    <div class="buttons" id="right">
       Information
     </div>
   </body>
