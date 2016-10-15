9c2dde11857dc6df373a9d230ee9421af5d79953
NotDavvid <notdavvid@gmail.com>
2016-10-15 01:00:12 -0400

    finished selection.php

--- selection/index.php
+++ selection/index.php
@@ -7,23 +7,50 @@
 </head>
 
   <body>
+    <div><img src="../resources/logo/logo.png"></div>
     <div id="am">I am:</div>
     <br>
     <hr>
     <br>
-    <div class="buttons">
+    <a onclick="click1()"><div class="buttons" id="survivor">
       Survivor
-    </div>
-    <div class="buttons">
+    </div></a>
+    <div class="buttons" id="co" onclick="click2()">
       Co-Survivor
     </div>
-    <div class="buttons" id="living">
-      Living With Metastatic Disease
+    <div class="buttons" id="living" onclick="click3()">
+      Living with Metastatic Disease
     </div>
     <div id="submit">
       Submit
     </div>
 
   </body>
-
+  <script>
+    function click1() {
+      document.getElementById("survivor").style.backgroundColor="#E877AF";
+      document.getElementById("co").style.color="black";
+      document.getElementById("survivor").style.color="white";
+      document.getElementById("co").style.backgroundColor="white";
+      document.getElementById("living").style.backgroundColor="white";
+      document.getElementById("living").style.color="black";
+      
+    }
+    function click2() {
+      document.getElementById("survivor").style.backgroundColor="white";
+      document.getElementById("survivor").style.color="black";
+      document.getElementById("co").style.backgroundColor="#E877AF";
+      document.getElementById("co").style.color="white";
+      document.getElementById("living").style.backgroundColor="white";
+      document.getElementById("living").style.color="black";
+    }
+    function click3() {
+      document.getElementById("survivor").style.backgroundColor="white";
+      document.getElementById("survivor").style.color="black";
+      document.getElementById("co").style.color="black";
+      document.getElementById("co").style.backgroundColor="white";
+      document.getElementById("living").style.backgroundColor="#E877AF";
+      document.getElementById("living").style.color="white";
+    }
+  </script>
 </html>
