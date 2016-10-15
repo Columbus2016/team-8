1266299f80ea2c601ce6fd844b9edb177ebddf27
NotDavvid <notdavvid@gmail.com>
2016-10-15 01:18:24 -0400

    preliminary css update

--- preliminary_information/index.php
+++ preliminary_information/index.php
@@ -5,17 +5,16 @@
   <link rel="stylesheet" href="style.css">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 </head>
-
   <body>
     <form action="action_page.php">
       Diagnosis Date:<br>
-      <input type="text" name="ddate" value="MM/YYYY">
+      <input type="text" name="ddate" value="MM/YYYY" class="input">
       <br>
       Cancer Type
       <br>
       
       
-      <input list="types" name="types">
+      <input list="types" name="types" class="input">
       <datalist id="types">
         <option value="Triple Negative Breast Cancer">
         <option value="HER2-positive breast cancer">
